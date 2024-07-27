824b246664d28e6e1554cd07265dc46e00a24fd7
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2024-07-27 19:58:55 +0530

    add polyfill of filter and includes method

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("Java Script");
 
+//  Map
+
+// In JavaScript, the map method is a powerful array method used to create a new array by applying a function to each element of an existing array.
+//  The original array remains unchanged.
+
 let arr = [10, 20, 30, 45, 60];
 
 function withMap(arr, mul) {
@@ -26,3 +31,56 @@ Array.prototype.myMap = function (callback) {
 
 let newArrResult = arr.myMap((ele, index, arr) => ele * 3);
 console.log(newArrResult);
+
+//  Filter
+// The filter method in JavaScript is used to create a new array with all elements that pass a test implemented by a provided function.
+//  This method does not change the original array.
+
+function filterWithNormalFun(arr, num) {
+  let newArr = [];
+  let l = arr.length;
+  for (let i = 0; i < l; i++) {
+    if (arr[i] > num) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(filterWithNormalFun(arr, 20));
+
+let filterData = arr.filter((ele, index, arr) => ele > 15);
+console.log(filterData);
+
+Array.prototype.myFilter = function (callBack) {
+  let newArr = [];
+  let l = this.length;
+  for (let i = 0; i < l; i++) {
+    if (callBack(this[i], i, this)) {
+      newArr.push(this[i]);
+    }
+  }
+  return newArr;
+};
+
+let newFilValue = arr.myFilter((ele) => ele > 30);
+console.log(newFilValue);
+
+// Includes
+// includes check value is present in array or not if value is there it return true else false
+
+let a = arr.includes(20);
+console.log(a);
+
+Array.prototype.myIncludes = function (val) {
+  let l = this.length;
+  for (let i = 0; i < l; i++) {
+    if (this[i] === val) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+let b = arr.myIncludes(10);
+console.log(b);
