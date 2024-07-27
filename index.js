b4d8a24b53425015fdcdd7ce1769e63563b690c3
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2024-07-27 19:14:06 +0530

    add some code in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Java Script')
+console.log("Java Script");
+
+let arr = [10, 20, 30, 45, 60];
+
+function withMap(arr, mul) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * mul);
+  }
+  return newArr;
+}
+console.log(withMap(arr, 2));
+
+let newArr = arr.map((ele, index, arr) => ele * 10);
+console.log(newArr);
+
+Array.prototype.myMap = function (callback) {
+  let newArr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    newArr.push(callback(this[i], i, this));
+  }
+  return newArr;
+};
+
+let newArrResult = arr.myMap((ele, index, arr) => ele * 3);
+console.log(newArrResult);
